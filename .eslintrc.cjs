1d8aba7e52541bdefe0de69f135c2eaf979b2785
theS1LV3R <me@s1lv3r.codes>
2022-11-06 21:06:44 +0100

    feat: add initial vue stuff, ready for dev

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'xo-typescript',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.config.json'],
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/naming-convention': 'off',
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="btn">Hello world</button>
+</template>
