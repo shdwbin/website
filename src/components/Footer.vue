f6c8451bfd0f6c92e8d6b4e0bdb93800fcdabf1c
theS1LV3R <me@s1lv3r.codes>
2022-11-11 00:04:46 +0100

    add social media buttons to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,13 +14,35 @@
         <Slogan />
       </p>
       <p class="font-bold">&copy; {{ year }} shdwbin.</p>
+      <div class="flex flex-row mt-4">
+        <a
+          v-for="social in socialMedia"
+          :key="social.link"
+          :href="social.link"
+          :aria-label="social.label"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="border-2 rounded-sm p-2 border-neutralShade">
+          <Octicon :name="social.icon" class="fill-neutralMain" />
+        </a>
+      </div>
     </div>
   </footer>
 </template>
 
 <script setup lang="ts">
   import ShadowbinIcon from '@/components/icons/ShadowbinIcon.vue';
+  import type { IconName } from '@primer/octicons';
+  import Octicon from './icons/Octicon.vue';
   import Slogan from './Slogan.vue';
 
   const year = new Date().getFullYear();
+
+  const socialMedia: Array<{ icon: IconName; link: string; label: string }> = [
+    {
+      icon: 'mark-github',
+      link: 'https://github.com/shdwbin',
+      label: 'Github',
+    },
+  ];
 </script>
