8faf55b69358bcb5a735c0ddaf897fe585fc1ac7
theS1LV3R <me@s1lv3r.codes>
2022-11-11 00:21:17 +0100

    fix gradient colors, due to accessibility concerns

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
       colors: {
         accentMain: '#000000',
         accentShade: '#101010',
-        accentTintStart: '#4A58D4',
-        accentTintEnd: '#7D4CA4',
+        accentTintStart: '#6672DB',
+        accentTintEnd: '#B396CF',
         neutralMain: '#EEEEEE',
         neutralShade: '#CACACA',
       },
