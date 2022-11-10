481e01be487c885f732d5ad191ff516f8c4dd7a3
theS1LV3R <me@s1lv3r.codes>
2022-11-10 21:35:13 +0100

    add octicon component

---
+++ src/components/icons/Octicon.vue
@@ -0,0 +1,17 @@
+<template>
+  <span v-html="svg" />
+</template>
+
+<script setup lang="ts">
+  import type { IconName, IconSize } from '@primer/octicons';
+  import octicons from '@primer/octicons';
+
+  const props = defineProps<{
+    name: IconName;
+    height?: IconSize;
+  }>();
+
+  const svg = octicons[props.name].toSVG({
+    height: props.height ?? 24,
+  });
+</script>
