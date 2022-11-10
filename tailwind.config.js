5937a9bfe50d6bdb9152c7186b518b16561dad9f
theS1LV3R <me@s1lv3r.codes>
2022-11-10 21:33:58 +0100

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{html,js,ts,tsx,vue,css,scss}', './index.html'],
   plugins: [require('daisyui')],
+  theme: {
+    extend: {
+      colors: {
+        accentMain: '#000000',
+        accentShade: '#101010',
+        accentTintStart: '#4A58D4',
+        accentTintEnd: '#7D4CA4',
+        neutralMain: '#EEEEEE',
+        neutralShade: '#CACACA',
+      },
+      borderRadius: {
+        sm: '10px',
+        md: '15px',
+        xl: '25px',
+      },
+    },
+  },
+  daisyui: {
+    prefix: 'd-',
+    themes: false,
+  },
 };
