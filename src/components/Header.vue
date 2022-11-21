07be0b4a8f82eba079a06dc0ddc0d3d4d979bc47
i0 <41456914+i0bs@users.noreply.github.com>
2022-11-21 17:29:47 -0500

    Adjust header content margins
    Signed-off-by: i0 <41456914+i0bs@users.noreply.github.com>

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="h-16 p-3 border-b border-accentShade">
-    <div class="w-5/6 max-w-5xl mx-auto">
+    <div class="w-5/6 max-w-6xl mx-auto">
       <ShadowbinIcon class="max-h-full m-auto float-left h-10" />
       <div class="float-right h-full flex flex-row items-center text-lg gap-4">
         <RouterLink to="/">Home</RouterLink>
