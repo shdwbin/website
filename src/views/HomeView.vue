3866c421dc8ef8fe7a547eb946c04a4dacd098df
theS1LV3R <me@s1lv3r.codes>
2022-11-25 23:20:59 +0100

    uncomment and use property instead

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,27 +3,27 @@
   <Section name="What We Do">
     <WhatWeDo />
   </Section>
-<!--  <Section name="How It's Done">-->
-<!--    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptates!-->
-<!--    Cumque perspiciatis earum, aut excepturi maiores voluptates harum possimus-->
-<!--    culpa temporibus corporis nulla quia at eaque expedita eligendi dignissimos-->
-<!--    tenetur corrupti unde explicabo! Facilis perspiciatis deserunt qui ab-->
-<!--    molestiae iusto recusandae blanditiis, eligendi delectus ad reiciendis.-->
-<!--    Quasi fugit aliquam, itaque dolorem obcaecati possimus minus. Ipsam alias-->
-<!--    exercitationem, dolore reiciendis, aliquid id sit illo ab beatae ratione ad-->
-<!--    ullam totam eaque cumque nulla sed, vitae nihil laudantium assumenda-->
-<!--    cupiditate. Aliquam, est necessitatibus? Assumenda praesentium illo-->
-<!--    blanditiis ex quo! Expedita nam quaerat odio hic id, eum, quae ea, excepturi-->
-<!--    velit et accusamus?-->
-<!--  </Section>-->
+  <Section name="How It's Done" hidden>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptates!
+    Cumque perspiciatis earum, aut excepturi maiores voluptates harum possimus
+    culpa temporibus corporis nulla quia at eaque expedita eligendi dignissimos
+    tenetur corrupti unde explicabo! Facilis perspiciatis deserunt qui ab
+    molestiae iusto recusandae blanditiis, eligendi delectus ad reiciendis.
+    Quasi fugit aliquam, itaque dolorem obcaecati possimus minus. Ipsam alias
+    exercitationem, dolore reiciendis, aliquid id sit illo ab beatae ratione ad
+    ullam totam eaque cumque nulla sed, vitae nihil laudantium assumenda
+    cupiditate. Aliquam, est necessitatibus? Assumenda praesentium illo
+    blanditiis ex quo! Expedita nam quaerat odio hic id, eum, quae ea, excepturi
+    velit et accusamus?
+  </Section>
   <Section name="Who We Are">
     <WeAreShadowbin class="mb-20" />
-<!--    <InvestorsSponsors />-->
+    <InvestorsSponsors class="hidden" />
   </Section>
-<!--  <Section name="Get Involved">-->
-<!--    <LatestPosts />-->
-<!--    <Explore />-->
-<!--  </Section>-->
+  <Section name="Get Involved" hidden>
+    <LatestPosts />
+    <Explore />
+  </Section>
 </template>
 
 <script setup lang="ts">
