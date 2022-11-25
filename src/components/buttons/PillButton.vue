a9af11911a4bc7528ceab4f286792b5f8e944630
theS1LV3R <me@s1lv3r.codes>
2022-11-25 23:20:15 +0100

    Add hover and disabled states to PillButton

--- src/components/buttons/PillButton.vue
+++ src/components/buttons/PillButton.vue
@@ -1,26 +1,28 @@
 <template>
-  <div
-    class="rounded-full p-1 px-5 flex flex-row flex-nowrap"
-    :class="`
-    ${bold ? 'font-bold' : ''}
-    ${
-      theme === 'white'
-        ? 'bg-neutralMain text-accentMain'
-        : theme === 'black'
-        ? 'bg-accentMain text-neutralMain'
-        : theme === 'gradient'
-        ? 'bg-gradient-to-r from-accentTintStart to-accentTintEnd text-neutralMain'
-        : theme === 'transparent'
-        ? 'bg-transparent border-2 border-accentShade dark:border-neutralShade'
-        : theme === 'inverted'
-        ? 'bg-neutralMain dark:bg-accentMain text-accentMain dark:text-neutralMain'
-        : 'bg-accentMain dark:bg-neutralMain text-neutralMain dark:text-accentMain'
-    }`">
+  <button
+    class="rounded-full p-1 px-5 flex flex-row flex-nowrap disabled:pointer-events-none"
+    :class="[
+      { 'font-bold': bold },
+      {
+        'bg-accentMain hover:bg-accentShade dark:bg-neutralMain dark:hover:bg-neutralShade text-neutralMain dark:text-accentMain':
+          !theme,
+        'bg-neutralMain text-accentMain hover:bg-neutralShade':
+          theme === 'white',
+        'bg-accentMain text-neutralMain hover:bg-accentShade':
+          theme === 'black',
+        'bg-gradient-to-r from-accentTintStart to-accentTintEnd text-neutralMain hover:shadow hover:shadow-gray-500 disabled:shadow-none':
+          theme === 'gradient',
+        'bg-transparent border-2 border-accentShade dark:border-neutralShade hover:border-accentMain dark:hover:border-neutralMain':
+          theme === 'transparent',
+        'bg-neutralMain dark:bg-accentMain text-accentMain dark:text-neutralMain':
+          theme === 'inverted',
+      },
+      {},
+    ]">
     <component
       :is="leftIcon"
       v-if="leftIcon"
       class="mr-1 ml-[-.5rem] max-h-full my-auto border-inherit text-inherit"
-      :name="leftIcon"
       role="presentation"
       aria-hidden="true" />
     <slot />
@@ -28,10 +30,9 @@
       :is="rightIcon"
       v-if="rightIcon"
       class="ml-1 mr-[-.5rem] max-h-full my-auto border-inherit text-inherit"
-      :name="rightIcon"
       role="presentation"
       aria-hidden="true" />
-  </div>
+  </button>
 </template>
 
 <script setup lang="ts">
@@ -41,6 +42,7 @@
     leftIcon?: Component;
     rightIcon?: Component;
     bold?: boolean;
+    hoverText?: string;
     theme?:
       | 'black'
       | 'white'
